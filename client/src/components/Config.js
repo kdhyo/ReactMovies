e02111ee25762951d2eb0e9bfa48b99deca1e4cc
dongHyo <kdhyo98@gmail.com>
2020-05-12 11:10:32 +0900

    master

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+
+export const API_KEY = "96f0d8778fc5a02a8e1cef3608a7412a";
+
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
